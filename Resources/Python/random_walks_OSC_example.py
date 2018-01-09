cc34109cfee889e9eaff88076b3dcff3408f6ca7
alejoe91 <alejoe9187@gmail.com>
2018-01-09 10:06:12 +0100

    random multi thread

--- Resources/Python/random_walks_OSC_example.py
+++ Resources/Python/random_walks_OSC_example.py
@@ -8,89 +8,123 @@ import OSC
 import time
 import numpy as np
 import matplotlib.pylab as plt
+import threading
+
+class OSCthread(threading.Thread):
+    def __init__(self, name, addr, port, freq, duration):
+        super(OSCthread, self).__init__(name=name)
+        self.osc = []
+        self.port = port
+        self.addr = addr
+        self.freq = freq
+        self.duration = duration
+        self.x = []
+        self.y = []
+        self.sent_trajectory = []
+        self.timestamps = []
+
+    
+    def setup(self, width, height):
+        self.osc = OSC.OSCClient()
+        self.osc.connect(('127.0.0.1', p))
+        self.nsamples = int(self.duration * freq)
+        self.period = 1. / float(freq)
+        self.width = width
+        self.height = height
+
+        print 'Created source: ', self.freq,  ' ', self.port, ' ', self.addr, ' ', self.period
+
+        x = []
+        y = []
+        x.append(np.random.uniform(0, 1))
+        y.append(np.random.uniform(0, 1))
+
+        while len(x) < self.nsamples:
+            dx, dy = 0.05 * np.random.randn(2)
+            x_n, y_n = (x[-1] + dx) * width, (y[-1] + dy) * height
+
+            while x_n < 0 or y_n < 0 or x_n > width or y_n > height:
+                dx, dy = 0.05 * np.random.randn(2)
+                x_n, y_n = (x[-1] + dx) * width, (y[-1] + dy) * height
+
+            x.append(x_n)
+            y.append(y_n)
+            
+        self.x = x
+        self.y = y
+    
+    def run(self):
+        prev_t = time.time()
+        count = 0
+        t0 = time.time()
+        try:
+            while True:
+                curr_t = time.time()
+
+                if curr_t - prev_t > self.period:
+                    if count < self.nsamples:
+                        m_x = self.x[count]
+                        m_y = self.y[count]
+                    else:
+                        print Exception('Finished sending samples for ' +  self.name )
+                        break
+
+                    # rand = np.random.rand(1)
+                    # if rand > 0.7:
+                    #     m_x = np.nan
+                    #     m_y = np.nan
+                    #
+                    # print m_x, m_y
+
+                    count += 1
+
+                    oscmsg = OSC.OSCMessage()
+                    oscmsg.setAddress(self.addr)
+                    oscmsg.append([m_x, m_y, width, height])
+                    self.sent_trajectory.append([m_x, m_y, width, height])
+                    self.timestamps.append(curr_t - t0)
+                    self.osc.send(oscmsg)
+
+                    prev_t = curr_t
+                    # print 'sent port: ', self.port, 'address: ', self.addr, curr_t - t0
+            t1 = time.time()
+            print 'finished in ', t1 - t0
+
+        except KeyboardInterrupt:
+            pass
+        
 
 n_sources = 10
 ports = range(27020, 27020 + n_sources)
 addresses = ['/red', '/green', '/blue',  '/magenta', '/cyan', '/orange', '/pink', '/grey', '/violet', '/yellow']
 frequencies = np.linspace(30, 300, n_sources)  # Hz
-duration = 60  # sec
+duration = 30  # sec
 nsamples = []
 osc_ports = []
 periods = []
 counts = []
 x_vec = []
 y_vec = []
-box_xlen = 1.
-box_ylen = 1.
 width = 1.
 height = 1.
 
-print 'Creating trajectories'
-for (p, addr, freq) in zip(ports, addresses, frequencies):
-    c = OSC.OSCClient()
-    c.connect(('127.0.0.1', p))
-    osc_ports.append(c)
-    nsamples.append(int(duration*freq))
-    periods.append(1./float(freq))
+osc = []
 
-    x = []
-    y = []
-    x.append(np.random.uniform(0, 1))
-    y.append(np.random.uniform(0, 1))
+for idx, (p, addr, freq) in enumerate(zip(ports, addresses, frequencies)):
+    thread = OSCthread(name = 'Tread' + str(idx), port=p, addr=addr, freq=freq, duration=duration)
+    thread.setup(width, height)
+    osc.append(thread)
 
-    while len(x) < nsamples[-1]:
-        dx, dy = 0.05*np.random.randn(2)
-        x_n, y_n = (x[-1] + dx) * box_xlen, (y[-1] + dy) * box_ylen
+ts = []
+data = []
 
-        while x_n < 0 or y_n < 0 or x_n > box_xlen or y_n > box_ylen:
-            dx, dy = 0.05*np.random.randn(2)
-            x_n, y_n = (x[-1] + dx) * box_xlen, (y[-1] + dy) * box_ylen
+for o in osc:
+    o.join()
+    print 'Finished: ', o.name
 
+    ts.append(np.array(o.timestamps))
+    data.append(np.array(o.sent_trajectory))
 
-        x.append(x_n)
-        y.append(y_n)
-
-    x_vec.append(x)
-    y_vec.append(y)
-
-counts = [0] * n_sources
-prev_t = [time.time()] * n_sources
-max_samples = np.max(nsamples)
-
-sent_trajectories = [[] for _ in range(n_sources)]
-
-print 'Sending trajectories'
-try:
-    while True:
-        curr_t = time.time()
-
-        for idx, c in enumerate(osc_ports):
-            if curr_t - prev_t[idx] > periods[idx]:
-
-                if counts[idx] < max_samples:
-                    m_x = x_vec[idx][counts[idx]]
-                    m_y = y_vec[idx][counts[idx]]
-                else:
-                    raise Exception('Finished sending samples')
-
-                rand = np.random.rand(1)
-                if rand > 0.7:
-                    m_x = np.nan
-                    m_y = np.nan
-
-                counts[idx] += 1
-
-                oscmsg = OSC.OSCMessage()
-                oscmsg.setAddress(addresses[idx])
-                oscmsg.append([m_x, m_y, width, height])
-                sent_trajectories[idx].append([m_x, m_y, width, height])
-                c.send(oscmsg)
-
-                prev_t[idx] = curr_t
-                print 'sent port: ', ports[idx] , 'address: ', addresses[idx]
-
-except KeyboardInterrupt:
-    pass
-
-
-
+# print 'Saving'
+# np.save('data/data_array', data)
+# np.save('data/timestamps', ts)
